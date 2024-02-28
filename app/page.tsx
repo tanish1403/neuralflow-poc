1fd347476660aaaf9187b5a6f7160154988f9c33
Ayush Kaithwas <ayush.91011@gmail.com>
2024-02-29 03:04:01 +0530

    Refactor page component and add new UI elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,52 @@
-"use client"
-import React from 'react';
-import ReactFlow from 'reactflow';
- 
-import 'reactflow/dist/style.css';
- 
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+"use client";
+import React from "react";
+import ReactFlow, { MarkerType, useEdgesState, useNodesState } from "reactflow";
 
+import "reactflow/dist/style.css";
+
+import CustomNode from "@/components/internal/CustomNode";
+import {
+  initialEdges,
+  initialNodes,
+} from "@/components/internal/initial-elements";
+import { LayoutMain } from "@/components/layout-main";
+import { Button } from "@/components/ui/button";
+import { Layers, Layers3, Play } from "lucide-react";
+
+const nodeTypes = {
+  custom: CustomNode,
+};
 
 export default function Home() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
   return (
-   <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
-    </div>
+    <LayoutMain>
+      <div className="absolute flex flex-col top-5 left-5 z-30 gap-2">
+        <Button
+          className="flex flex-col justify-around h-[5rem] w-[5rem]"
+          variant={"outline"}
+        >
+          <Play fill="black" />
+          <h2>Input</h2>
+        </Button>
+        <Button
+          className="flex flex-col justify-around h-[5rem] w-[5rem]"
+          variant={"outline"}
+        >
+          <Layers3 />
+          <h2>Layer</h2>
+        </Button>
+      </div>
+      <ReactFlow
+        nodes={initialNodes}
+        edges={initialEdges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        fitView
+        nodeTypes={nodeTypes}
+      />
+    </LayoutMain>
   );
 }
