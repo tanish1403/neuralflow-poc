befe5df7db7816896bc2ed56fe8b9aad1a9556d6
Ayush Kaithwas <ayush.91011@gmail.com>
2024-02-29 03:03:02 +0530

    Refactor base-layer.ts to export Layer class and update string join separator to be space separated

--- packages/tf/base-layer.ts
+++ packages/tf/base-layer.ts
@@ -10,11 +10,11 @@ class Args {
     return this.value
       .split("_")
       .map((part) => part.charAt(0).toUpperCase() + part.slice(1).toLowerCase())
-      .join("");
+      .join(" ");
   }
 }
 
-class Layer {
+export class Layer {
   name: string;
   nameTf: string;
   args: Args[];
