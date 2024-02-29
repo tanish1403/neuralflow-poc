79ed7c830acdbfa9a08a0723615fc4e3adc044bc
Ayush Kaithwas <ayush.91011@gmail.com>
2024-02-29 17:57:09 +0530

    Remove unused import and edge in initial-elements.ts

--- components/internal/initial-elements.ts
+++ components/internal/initial-elements.ts
@@ -1,20 +1,4 @@
-import { dense } from "@/packages/tf/layers/dense";
-import { MarkerType, Position } from "reactflow";
-
 export const initialNodes = [];
 
 export const initialEdges = [
-  {
-    id: "e4-5",
-    source: "4",
-    target: "5",
-    type: "smoothstep",
-    sourceHandle: "handle-0",
-    data: {
-      selectIndex: 0,
-    },
-    markerEnd: {
-      type: MarkerType.ArrowClosed,
-    },
-  },
 ];
