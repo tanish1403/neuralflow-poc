23d5ca56d420c3fdf866010625087346f1addce1
Ayush Kaithwas <ayush.91011@gmail.com>
2024-03-01 04:00:28 +0530

    Refactor CustomNode component to use an accordion for parameters

--- components/internal/CustomNode.tsx
+++ components/internal/CustomNode.tsx
@@ -3,12 +3,18 @@ import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Layer as L } from "@/packages/tf/types";
 import { Handle, NodeProps, Position } from "reactflow";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 function CustomNode(props: NodeProps<L>) {
   const { data, isConnectable } = props;
 
   return (
-    <div className="flex flex-col text-xs bg-gray-100 border active:border-orange-300">
+    <div className="flex flex-col text-xs bg-gray-100 border active:border-orange-300 rounded-sm">
       <Handle
         id="a"
         type="target"
@@ -17,19 +23,30 @@ function CustomNode(props: NodeProps<L>) {
         onConnect={(params) => console.log("handle onConnect", params)}
         isConnectable={isConnectable}
       />
-      <div className="font-bold border-b-white border-b-2 p-2">
-        <h2>{data.name}</h2>
-      </div>
-      <div className="px-2 pb-2">
-        {data.args.map((arg) => {
-          return (
-            <div key={arg.GetName()}>
-              <Label className="text-[0.5rem] m-0">{arg.GetName()}</Label>
-              <Input className="text-[0.5rem] h-[20px]" />
+
+      <Accordion className="" type="single" collapsible>
+        <AccordionItem className="flex flex-col items-center" value="item-1">
+          <h2 className="text-center font-bold border-b-white border-b-2 p-3 w-full">
+            {data.name}
+          </h2>
+          <AccordionTrigger className="flex items-center gap-1 text-[0.5rem] p-3">
+            Parameters
+          </AccordionTrigger>
+          <AccordionContent className="">
+            <div className="px-2 pb-2">
+              {data.args.map((arg) => {
+                return (
+                  <div key={arg.GetName()}>
+                    <Label className="text-[0.5rem] m-0">{arg.GetName()}</Label>
+                    <Input className="text-[0.5rem] h-[20px]" />
+                  </div>
+                );
+              })}
             </div>
-          );
-        })}
-      </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
       <Handle
         id="b"
         type="source"
