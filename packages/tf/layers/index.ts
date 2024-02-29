c70a6df65be34df437d75e334dbc4d42a88ea26a
Ayush Kaithwas <ayush.91011@gmail.com>
2024-02-29 17:44:07 +0530

    Refactor export layers

--- packages/tf/layers/index.ts
+++ packages/tf/layers/index.ts
@@ -1,2 +1,8 @@
-export * from "./batch_normalisation";
-export * from "./activation";
+import { activation } from "./activation";
+import { batch_normalisation } from "./batch_normalisation";
+import { dense } from "./dense";
+export * from "./input";
+
+export const layers = [
+    batch_normalisation, dense, activation
+]
