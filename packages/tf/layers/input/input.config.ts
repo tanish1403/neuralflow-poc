6092d16a0fcf02c3968fdc1e131e5fb577ddf02c
Ayush Kaithwas <ayush.91011@gmail.com>
2024-03-08 22:57:49 +0530

    add static of method on tuple

--- packages/tf/layers/input/input.config.ts
+++ packages/tf/layers/input/input.config.ts
@@ -1,24 +1,10 @@
-// shape=None,
-// batch_size=None,
-// name=None,
-// dtype=None,
-// sparse=None,
-// tensor=None,
-// ragged=None,
-
 import { Tuple } from "@/packages/typewriter";
 import { ArgsInstance } from "../../base-layer";
 
-// type_spec=None,
 export const args: ArgsInstance[] = [
   {
     name: "shape",
     isRequired: false,
     defaultValue: new Tuple("None", "None"),
   },
-  // {
-  //   name: "batch_size",
-  //   isRequired: false,
-  //   defaultValue: "None",
-  // },
 ];
--- packages/typewriter/tuple/index.ts
+++ packages/typewriter/tuple/index.ts
@@ -1,13 +1,19 @@
 import { p_primitive_types } from "../python_primitive";
 
-export class Tuple extends Array<p_primitive_types> {
+export class Tuple {
   // Constructor to initialize the Tuple
+  value: p_primitive_types[];
   constructor(...elements: p_primitive_types[]) {
-    super(...elements);
+    this.value = elements;
   }
 
   // Method to convert the Tuple into a code string format
   toCodeString(): string {
-    return `(${this.join(",")})`;
+    return `(${this.value.join(",")})`;
+  }
+
+  // Static method for creating a new Tuple instance
+  static of(...elements: p_primitive_types[]): Tuple {
+    return new Tuple(...elements);
   }
 }
